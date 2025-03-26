f599efb262d627925ad195a0a6d040ed1f7738d6
Radhofan <radhofanazizi@gmail.com>
2025-03-26 21:09:42 +0700

    ada

---
+++ reproduce.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Install Miniconda
+curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda <<< "Yes"
+
+# Set environment variables
+export PATH="$HOME/miniconda/bin:$PATH"
+export MAMBA_ROOT_PREFIX="$HOME/miniconda"
+
+# Install Mamba
+conda install -c conda-forge mamba -y
+
+# Initialize Mamba shell
+mamba shell init --shell=bash
+source ~/.bashrc  # Reload shell config
+eval "$(mamba shell hook --shell=bash)"
+
+# Create and activate conda environment
+mamba create -n fairify -y
+source $HOME/miniconda/bin/activate fairify
+mamba activate fairify
+
+# install requirements
+pip install -r Fairify/requirements.txt
+
+# Run normal
+bash Fairify/src/fairify.sh AC
+
+# Run stress
+bash Fairify/stress/fairify-stress.sh AC
+
+# Run relaxed
+bash Fairify/relaxed/fairify-relaxed.sh AC
+
+# Run targeted
+bash Fairify/targeted/fairify-targeted.sh AC

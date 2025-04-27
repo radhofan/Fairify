180813f638e893f370bdf37c89c3d1b64fc4abe7
Radhofan <radhofanazizi@gmail.com>
2025-04-27 09:02:06 +0700

    ada

--- src/GC/Verify-GC-experiment.py
+++ src/GC/Verify-GC-experiment.py
@@ -377,7 +377,7 @@ for model_file in tqdm(model_files, desc="Processing Models"):  # tqdm for model
         y_pred = pd.Series(np.array(y_pred).ravel())  
         prot_attr = pd.Series(np.array(prot_attr).ravel())
 
-        X_test_copy = pd.Series(X_test)
+        X_test_copy = pd.DataFrame(X_test)
         dataset = pd.concat([X_test_copy , y_true], axis=1)
         dataset_pred = pd.concat([X_test_copy , y_pred], axis=1)
         unprivileged_groups = [{'age': 0}]

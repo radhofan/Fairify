08eccd97facd00018dcd96d24832fb4cff344d29
Radhofan <radhofanazizi@gmail.com>
2025-04-27 09:12:54 +0700

    ada

--- src/GC/Verify-GC-experiment.py
+++ src/GC/Verify-GC-experiment.py
@@ -381,8 +381,10 @@ for model_file in tqdm(model_files, desc="Processing Models"):  # tqdm for model
         X_test_copy = pd.DataFrame(X_test)
         dataset = pd.concat([X_test_copy , y_true], axis=1)
         dataset_pred = pd.concat([X_test_copy , y_pred], axis=1)
-        dataset = BinaryLabelDataset(df=dataset, label_names=['class'], protected_attribute_names=['age'])
-        dataset_pred = BinaryLabelDataset(df=dataset_pred, label_names=['class'], protected_attribute_names=['age'])
+        print("Columns in dataset:", dataset.columns)
+        print("Columns in dataset_pred:", dataset_pred.columns)
+        dataset = BinaryLabelDataset(df=dataset, label_names=['credit'], protected_attribute_names=['age'])
+        dataset_pred = BinaryLabelDataset(df=dataset_pred, label_names=['credit'], protected_attribute_names=['age'])
         unprivileged_groups = [{'age': 0}]
         privileged_groups = [{'age': 1}]
         classified_metric = ClassificationMetric(dataset,

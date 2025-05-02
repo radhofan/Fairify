541261619d43a39db9b825bffb2468f3cafc7b6c
Radhofan <radhofanazizi@gmail.com>
2025-05-02 16:14:23 +0700

    ada

--- src/GC/Verify-GC-experiment.py
+++ src/GC/Verify-GC-experiment.py
@@ -490,7 +490,7 @@ for model_file in tqdm(model_files, desc="Processing Models"):  # tqdm for model
         ti = classified_metric.theil_index()
 
         # Save metric to csv
-        model_prefix = next((prefix for prefix in ["GC-2", "GC-8"] if model_file.startswith(prefix)), "unknown")
+        model_prefix = next((prefix for prefix in ["GC-1", "GC-8"] if model_file.startswith(prefix)), "unknown")
         file_name = f"{result_dir}synthetic-german-predicted-{model_prefix}-metrics.csv"
         cols = ['Partition ID', 'Original Accuracy', 'Original F1 Score', 'Pruned Accuracy', 'Pruned F1', 'DI', 'SPD', 'EOD', 'AOD', 'ERD', 'CNT', 'TI']
         data_row = [partition_id, orig_acc, orig_f1, pruned_acc, pruned_f1, di, spd, eod, aod, erd, cnt, ti]

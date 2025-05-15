6113f9b9004c88c2998cbe3bd332f4ed0f92c2a8
Radhofan <radhofanazizi@gmail.com>
2025-05-15 12:24:46 +0700

    ada

--- src/CP/Verify-CP.py
+++ src/CP/Verify-CP.py
@@ -72,6 +72,7 @@ p_dict = partition(range_dict, PARTITION_THRESHOLD)
 p_list = partitioned_ranges(A, PA, p_dict, range_dict)
 print('Number of partitions: ', len(p_list))
 shuffle(p_list)
+print("p_list contents:", p_list)
 
 # In[]
 

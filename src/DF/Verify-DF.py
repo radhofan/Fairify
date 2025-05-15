76a59d889c432cd7cd31a78161afd78ebeb2bb89
Radhofan <radhofanazizi@gmail.com>
2025-05-15 19:18:14 +0700

    ada

--- src/DF/Verify-DF.py
+++ src/DF/Verify-DF.py
@@ -27,11 +27,17 @@ from aif360.datasets import *
 
 # In[]
 
-print("before loading...")
+print("before loading...", flush=True)
 df, X_train, y_train, X_test, y_test = load_default()
-print("All columns after encoding and dropping:", list(df.columns))
+print("after load_default()", flush=True)
+
+print("All columns after encoding and dropping:", list(df.columns), flush=True)
+
 X = np.r_[X_train, X_test]
-single_input = X_test[0].reshape(1, 24)
+print("Concatenated X", flush=True)
+
+single_input = X_test[0].reshape(1, 30)
+print("Reshaped single_input:", single_input.shape, flush=True)
 #print_metadata(df)
 
 # In[]

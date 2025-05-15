95cd0460f18e923b92bf974a8814991cdc2b8a2e
Radhofan <radhofanazizi@gmail.com>
2025-05-15 11:57:22 +0700

    ada

--- utils/CP-1-Model-Functions.py
+++ utils/CP-1-Model-Functions.py
@@ -31,13 +31,22 @@ def net(x, w, b):
     return x3
 
 def z3_net(x, w, b):
-    input_size = len(x)
+    # The error shows that the weight matrix expects 12 inputs,
+    # but we're providing 20 dimensions
     
-    fl_x = np.array([FP(f'fl_x{i}', Float32()) for i in range(input_size)])
+    # Instead of using the length of the input vector, we should use
+    # the expected input size from the weight matrix dimension
+    expected_input_size = w[0].shape[0]  # This should be 12 based on the error
     
-    for i in range(input_size):
+    # Create Z3 floating-point variables based on the expected input size
+    fl_x = np.array([FP(f'fl_x{i}', Float32()) for i in range(expected_input_size)])
+    
+    # Handle the case where input dimensions don't match
+    # Only use the first expected_input_size elements from x
+    for i in range(min(len(x), expected_input_size)):
         fl_x[i] = ToReal(x[i])
     
+    # Forward propagation through the network
     x1 = w[0].T @ fl_x + b[0]
     y1 = z3Relu(x1)
     x2 = w[1].T @ y1 + b[1]

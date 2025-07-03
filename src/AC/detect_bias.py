f51ac5152628cc138c835675ca445513f0488812
Radhofan <radhofanazizi@gmail.com>
2025-07-03 09:04:39 +0700

    ada

--- src/AC/detect_bias.py
+++ src/AC/detect_bias.py
@@ -149,11 +149,25 @@ df_synthetic.dropna(inplace=True)
 cat_feat = ['sex', 'workclass', 'education', 'marital-status',
             'occupation', 'relationship', 'native-country', 'race']
 
+label_order_map = {
+    'sex': ['Female', 'Male'],
+    'workclass': ['Federal-gov', 'Local-gov', 'Never-worked', 'Private', 'Self-emp-inc', 'Self-emp-not-inc', 'State-gov', 'Without-pay'],
+    'education': ['10th', '11th', '12th', '1st-4th', '5th-6th', '7th-8th', '9th', 'Assoc-acdm', 'Assoc-voc', 'Bachelors', 'Doctorate', 'HS-grad', 'Masters', 'Preschool', 'Prof-school', 'Some-college'],
+    'marital-status': ['Divorced', 'Married-AF-spouse', 'Married-civ-spouse', 'Married-spouse-absent', 'Never-married', 'Separated', 'Widowed'],
+    'occupation': ['Adm-clerical', 'Armed-Forces', 'Craft-repair', 'Exec-managerial', 'Farming-fishing', 'Handlers-cleaners', 'Machine-op-inspct', 'Other-service', 'Priv-house-serv', 'Prof-specialty', 'Protective-serv', 'Sales', 'Tech-support', 'Transport-moving'],
+    'relationship': ['Husband', 'Not-in-family', 'Other-relative', 'Own-child', 'Unmarried', 'Wife'],
+    'native-country': ['Cambodia', 'Canada', 'China', 'Columbia', 'Cuba', 'Dominican-Republic', 'Ecuador', 'El-Salvador', 'England', 'France', 'Germany', 'Greece', 'Guatemala', 'Haiti', 'Holand-Netherlands', 'Honduras', 'Hong', 'Hungary', 'India', 'Iran', 'Ireland', 'Italy', 'Jamaica', 'Japan', 'Laos', 'Mexico', 'Nicaragua', 'Outlying-US(Guam-USVI-etc)', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto-Rico', 'Scotland', 'South', 'Taiwan', 'Thailand', 'Trinadad&Tobago', 'United-States', 'Vietnam', 'Yugoslavia'],
+    'race': ['Amer-Indian-Eskimo', 'Asian-Pac-Islander', 'Black', 'Other', 'White']
+}
+
+cat_feat = list(label_order_map.keys())
+
 for feature in cat_feat:
     le = LabelEncoder()
-    df_synthetic[feature] = le.fit_transform(df_synthetic[feature])
+    le.classes_ = np.array(label_order_map[feature])
+    df_synthetic[feature] = le.transform(df_synthetic[feature])
 
-# Discretize numerical features
+# Discretize numerical features consistently
 binning_cols = ['capital-gain', 'capital-loss']
 for feature in binning_cols:
     bins = KBinsDiscretizer(n_bins=20, encode='ordinal', strategy='uniform')

4272fe7ac630f9c0ee320c0870dfd648b0faa4cd
Radhofan <radhofanazizi@gmail.com>
2025-07-06 18:52:18 +0700

    ada

--- reproduce-experiment.sh
+++ reproduce-experiment.sh
@@ -34,8 +34,8 @@ python Fairify/src/AC/detect_bias.py
 python Fairify/src/AC/metrics.py
 # bash Fairify/src/fairify.sh Fairify/src/AC/Verify-AC-experiment-new2.py
 
-# python Fairify/src/GC/metrics.py
 # python Fairify/src/GC/detect_bias.py
+# python Fairify/src/GC/metrics.py
 # bash Fairify/src/fairify.sh Fairify/src/GC/Verify-GC-experiment-new2.py
 
 # python Fairify/src/BM/detect_bias.py

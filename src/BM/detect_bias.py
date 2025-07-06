d8c66416d107be15fa08a0e97fb40304a36389b9
Radhofan <radhofanazizi@gmail.com>
2025-07-06 14:48:19 +0700

    ada

--- src/BM/detect_bias.py
+++ src/BM/detect_bias.py
@@ -402,7 +402,7 @@ def masked_train_step(x, y, model, optimizer, neuron_masks):
     return loss
 
 # Compile model
-optimizer = Adam(learning_rate=0.1)
+optimizer = Adam(learning_rate=0.00001)
 # BM-1 = 0.0001 
 # BM-2 =  
 original_model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])

9342f5eabf15876d847ade07ea169127bcff1dac
Radhofan <radhofanazizi@gmail.com>
2025-07-07 09:57:45 +0700

    ada

--- src/AC/detect_bias.py
+++ src/AC/detect_bias.py
@@ -254,7 +254,7 @@ def masked_train_step(x, y, model, optimizer, neuron_masks):
     return loss
 
 # Compile model
-optimizer = Adam(learning_rate=0.000001)
+optimizer = Adam(learning_rate=0.000005)
 # AC-1 = 0.000015
 # AC-2 = 0.0000001
 # AC-3 = 0.0000009

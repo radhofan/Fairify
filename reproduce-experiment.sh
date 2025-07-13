4ca524009f37beafa2ebbec22f3b280b69a1f5df
Radhofan <radhofanazizi@gmail.com>
2025-07-13 16:52:49 +0700

    ada

--- reproduce-experiment.sh
+++ reproduce-experiment.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
 
-# Install Miniconda
 curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda <<< "Yes"
 
-# Set environment variables
 export PATH="$HOME/miniconda/bin:$PATH"
 export MAMBA_ROOT_PREFIX="$HOME/miniconda"
 
-# Install Mamba
 conda install -c conda-forge mamba -y
 
-# Initialize Mamba shell
 mamba shell init --shell=bash
-source ~/.bashrc  # Reload shell config
+source ~/.bashrc  
 eval "$(mamba shell hook --shell=bash)"
 
-# Create and activate conda environment
 mamba create -n fairify python=3.9 -y
 source $HOME/miniconda/bin/activate fairify
 mamba activate fairify
 
-# install requirements
 pip install -r Fairify/requirements.txt
 pip install tqdm
 sudo apt install csvtool
@@ -31,7 +25,10 @@ sudo apt install -y python3-swiftclient
 # python Fairify/src/AC/new_model_2.py
 
 python Fairify/src/AC/train_fair_model.py
-python Fairify/src/AC/metrics.py
+python Fairify/src/AC/metric_aif360.py
+python Fairify/src/AC/metric_themis_causal.py
+python Fairify/src/AC/metric_themis_unfairness.py
+# python Fairify/src/AC/metrics.py
 # bash Fairify/src/fairify.sh Fairify/src/AC/Verify-AC-experiment-v2.py
 
 # python Fairify/src/GC/train_fair_model.py

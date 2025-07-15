b2191875f5da4b7d148e6ba0daf18b8453975f6d
Radhofan <radhofanazizi@gmail.com>
2025-07-15 10:48:28 +0700

    ada

--- src/GC/train_fair_model.py
+++ src/GC/train_fair_model.py
@@ -18,8 +18,8 @@ import tensorflow as tf
 from collections import defaultdict
 
 # Model paths
-ORIGINAL_MODEL_NAME = "GC-3"
-FAIRER_MODEL_NAME = "GC-3-Retrained"
+ORIGINAL_MODEL_NAME = "GC-2"
+FAIRER_MODEL_NAME = "GC-2-Retrained"
 
 # Load pre-trained adult model
 print("Loading original model...")

5d5a0ec335710bbf5a00949c6afbb06a40eacea5
Radhofan <radhofanazizi@gmail.com>
2025-07-19 14:54:32 +0700

    ada

--- utils/AC-13-Model-Functions.py
+++ utils/AC-13-Model-Functions.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# symbolic translation of the 3-layer Keras model into z3
-
+# reinterpret network symbolically using z3 variables.
 import sys
 from z3 import *
-import numpy as np
-import pandas as pd
+import numpy as np 
+import pandas as pd 
 import collections
 import time
 import datetime
@@ -15,14 +14,13 @@ from utils.verif_utils import *
 
 
 def layer_net(x, w, b):
-    layers = []
+    layers = []    
     for i in range(len(w)):
         x1 = w[i].T @ x + b[i]
-        y1 = x1 if i == len(w) - 1 else relu(x1)
+        y1 = x1 if i == len(w)-1 else relu(x1)
         layers.append(y1)
         x = y1
     return layers
-
 
 def net(x, w, b):
     x1 = w[0].T @ x + b[0]
@@ -30,19 +28,21 @@ def net(x, w, b):
     x2 = w[1].T @ y1 + b[1]
     y2 = relu(x2)
     x3 = w[2].T @ y2 + b[2]
-    return x3  # pre-sigmoid
-
+    # y = 1 / (1 + math.exp(-x3)) # WP computer for sigmoid
+    return x3
 
 def z3_net(x, w, b):
-    fl_x = np.array([FP(f'fl_x{i}', Float32()) for i in range(13)])
+    
+    fl_x = np.array([FP('fl_x%s' % i, Float32()) for i in range(13)])  
+    
     for i in range(len(x)):
         fl_x[i] = ToReal(x[i])
-
+    
+        
     x1 = w[0].T @ fl_x + b[0]
     y1 = z3Relu(x1)
-
     x2 = w[1].T @ y1 + b[1]
     y2 = z3Relu(x2)
-
     x3 = w[2].T @ y2 + b[2]
-    return x3  # pre-sigmoid
+    # y = 1 / (1 + math.exp(-x3)) # WP computer for sigmoid
+    return x3

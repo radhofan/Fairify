85c17eacf36aaf8ac706521b59aa8cf445e7f382
Radhofan <radhofanazizi@gmail.com>
2025-07-24 15:39:32 +0700

    ada

--- src/AC/metric_themis_causality.py
+++ src/AC/metric_themis_causality.py
@@ -201,8 +201,8 @@ if __name__ == "__main__":
     # ORIGINAL_MODEL_NAME = "AC-14-Biased" 
     # FAIRER_MODEL_NAME = "AC-14-Biased-Retrained" 
     
-    ORIGINAL_MODEL_NAME = "AC-15"        
-    FAIRER_MODEL_NAME = "AC-15-Retrained"
+    # ORIGINAL_MODEL_NAME = "AC-15"        
+    # FAIRER_MODEL_NAME = "AC-15-Retrained"
     
     # ORIGINAL_MODEL_NAME = "AC-15-Biased" 
     # FAIRER_MODEL_NAME = "AC-15-Biased-Retrained" 

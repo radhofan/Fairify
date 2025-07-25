8e1b7f9dee698ddb4f3ee4a935e821c748435a65
Radhofan <radhofanazizi@gmail.com>
2025-07-25 17:37:01 +0700

    ada

--- src/AC/Verify-AC-experiment-v2.py
+++ src/AC/Verify-AC-experiment-v2.py
@@ -163,7 +163,7 @@ for model_file in tqdm(model_files, desc="Processing Models"):  # tqdm for model
     # if not model_file.startswith("AC-15-Biased."): # Done
     #     continue
 
-    if not model_file.startswith("AC-10."): # Done
+    if not model_file.startswith("AC-11."): # Done
         continue
 
     print('==================  STARTING MODEL ' + model_file)

8329875b2e91e7316836e254eb270bcc46b3113a
Radhofan <radhofanazizi@gmail.com>
2025-07-26 11:23:24 +0700

    ada

--- src/AC/train_fair_model.py
+++ src/AC/train_fair_model.py
@@ -364,7 +364,7 @@ FAIRER_MODEL_NAME = "AC-5-Retrained"
 # ORIGINAL_MODEL_NAME = "AC-15-Biased" 
 # FAIRER_MODEL_NAME = "AC-15-Biased-Retrained" 
 
-learning_rate = 0.00008
+learning_rate = 0.0001
 # AC-1 = 0.000015
 # AC-2 = 0.0000001
 # AC-3 = 0.000003

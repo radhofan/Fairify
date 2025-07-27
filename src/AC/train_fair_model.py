bdd5c5b4117bd0f70c201136e2b47b1914f73dba
Radhofan <radhofanazizi@gmail.com>
2025-07-27 13:35:40 +0700

    ada

--- src/AC/train_fair_model.py
+++ src/AC/train_fair_model.py
@@ -342,8 +342,8 @@ import tensorflow as tf
 from collections import defaultdict
 
 
-ORIGINAL_MODEL_NAME = "AC-14"
-FAIRER_MODEL_NAME = "AC-14-Retrained"
+ORIGINAL_MODEL_NAME = "AC-15"
+FAIRER_MODEL_NAME = "AC-15-Retrained"
 
 # ORIGINAL_MODEL_NAME = "AC-13"         
 # FAIRER_MODEL_NAME = "AC-13-Retrained" 
@@ -364,7 +364,7 @@ FAIRER_MODEL_NAME = "AC-14-Retrained"
 # ORIGINAL_MODEL_NAME = "AC-15-Biased" 
 # FAIRER_MODEL_NAME = "AC-15-Biased-Retrained" 
 
-learning_rate = 0.001
+learning_rate = 0.0001
 # AC-1 = 0.000015
 # AC-2 = 0.0000001
 # AC-3 = 0.000003
@@ -378,7 +378,7 @@ learning_rate = 0.001
 # AC-11 = 0.0000005
 # AC-12 = 0.00001
 # AC-13 = 0.000001
-# AC-14 = 0.0001
+# AC-14 = 0.001
 # AC-15 = 0.000001
 # AC-16 = 0.000001
 # AC-17 = 0.000001

d31bb56947b069c9e4528f89f4d8f5542f82b113
Radhofan <radhofanazizi@gmail.com>
2025-07-30 09:16:57 +0700

    ada

--- reproduce-experiment.sh
+++ reproduce-experiment.sh
@@ -39,13 +39,13 @@ pip install torch torchvision torchaudio --index-url https://download.pytorch.or
 # python Fairify/src/BM/metric_aif360.py
 # python Fairify/src/BM/metric_themis_causality.py
 # python Fairify/src/BM/metric_random_unfairness.py
-bash Fairify/src/fairify.sh Fairify/src/BM/Verify-BM-experiment-v2.py
+# bash Fairify/src/fairify.sh Fairify/src/BM/Verify-BM-experiment-v2.py
 
 # python Fairify/src/GC/train_fair_model.py
 # python Fairify/src/GC/metric_aif360.py
 # python Fairify/src/GC/metric_themis_causality.py
 # python Fairify/src/GC/metric_random_unfairness.py
-# bash Fairify/src/fairify.sh Fairify/src/GC/Verify-GC-experiment-v2.py
+bash Fairify/src/fairify.sh Fairify/src/GC/Verify-GC-experiment-v2.py
 
 # Run fairify experiment
 # bash Fairify/src/fairify.sh Fairify/src/GC/Verify-GC-experiment.py
